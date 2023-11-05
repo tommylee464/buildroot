f39ab79fd4961e37bec373a04336220876a42ada
tommylee464 <rancourttommylee@gmail.com>
2023-11-05 13:34:25 -0500

    Fix

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -10,10 +10,10 @@ BISON_SITE = $(BR2_GNU_MIRROR)/bison
 BISON_LICENSE = GPL-3.0+
 BISON_LICENSE_FILES = COPYING
 BISON_CPE_ID_VENDOR = gnu
-# parallel build issue in examples/c/reccalc/
 BISON_MAKE = $(MAKE1)
 HOST_BISON_DEPENDENCIES = host-m4
 HOST_BISON_CONF_OPTS = --enable-relocatable
 HOST_BISON_CONF_ENV = ac_cv_libtextstyle=no
 
+$(eval $(autotools-package))
 $(eval $(host-autotools-package))
